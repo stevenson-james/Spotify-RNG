2eccebbb4922649bbdac560ba423d80553498e5d
James Stevenson <jpatstevenson@gmail.com>
2021-03-09 00:27:14 -0800

    Update init.py file

--- init.py
+++ init.py
@@ -4,4 +4,7 @@ import json
 with open('config.json') as f:
   config = json.load(f)
 
-print(config["SPOTIPY_CLIENT_ID"])
+os.system('set FLASK_APP=app.py')
+os.system('set SPOTIPY_CLIENT_ID=' + config['SPOTIPY_CLIENT_ID'])
+os.system('set SPOTIPY_CLIENT_SECRET=' + config['SPOTIPY_CLIENT_SECRET'])
+os.system('set SPOTIPY_REDIRECT_URI=' + config['SPOTIPY_REDIRECT_URI'])
